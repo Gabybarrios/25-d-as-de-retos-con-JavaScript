5daccfdefe62c8013f65052053f320dc83af037c
Gaby Barrios <gabybarriosdesignweb@gmail.com>
2022-09-15 16:26:16 -0300

    feat: filtrar ovejas por nombre y color

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'},
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo'}
+  ]
+
+function contarOvejas(ovejas) {
+    return ovejas.filter(oveja => oveja.name.toLowerCase().includes('n') && oveja.name.toLowerCase().includes('a') && oveja.color === 'rojo' )
+  }
+
+console.log(contarOvejas(ovejas))
